9ad928a340f82fb33283ab79ef615be7b0971cba
lanchaolve <351353060@qq.com>
2015-08-26 17:39:07 +0800

    1

--- 北京赛车/Program.cs
+++ 北京赛车/Program.cs
@@ -16,7 +16,7 @@ namespace 北京赛车
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form1()); 
         }
     }
 }
